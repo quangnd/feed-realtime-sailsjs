25ae308e917850f623b5801189b76e3accd87227
Quang Nguyen <quangnd.edu@gmail.com>
2018-01-31 16:09:41 +0700

    Add Create button and faker

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # feed-realtime-sailsjs
 
-A simple feed realtime website with BlueprintAPI and Sails Socket.IO (for demo)
+A simple feed realtime website with BlueprintAPI, Sails Socket.IO and Faker 😈
 
 ## Up & Running
 
 1. Run `npm install`.
 2. Run `sails lift`.
-3. Go to `http://localhost:1337/feed/create?icon=fuck&title=Mun%20is%20me&description=Do%20not%20hesitate%20to%20try` to create a new feed (BlueprintAPI).
-4. Go to `http://localhost:1337` to see the results.
+3. Go to `http://localhost:1337` to see the results.
 
-Note: You should open some browser windows to see the socket effect.
+*Note: You should open some browser windows to see the socket effect. The data will reset when you restart Sails server*
 
 Happy learning 😁
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,25 @@
 var feedApp = angular.module('feedApp', ['ngResource', 'angularMoment', 'ngAnimate']);
 
-
-feedApp.controller('FeedCtrl', ['$scope', '$resource', '$timeout', function($scope, $resource, $timeout) {
+feedApp.controller('FeedCtrl', ['$scope', '$resource', '$timeout', '$http', function($scope, $resource, $timeout, $http) {
   $scope.feedEntries = $resource('/feed').query();
 
   io.socket.get('/feed/subscribe', function(data, jwr) {
     io.socket.on('new_entry', function(entry) {
-    	console.log('on new_entry event');
-    	console.log(entry);
       $timeout(function() {
         $scope.feedEntries.unshift(entry);
       });
     });
   });
+
+  $scope.createNewFeed = function() {
+    var icon = 'heart';
+    var title = faker.name.jobTitle();
+    var description = faker.name.jobDescriptor();
+    var apiUrl = '/feed' + '/create?icon=' + icon + '&title='+ title + '&description=' + description;
+
+    $http.get(apiUrl)
+      .then(function(response) {
+        //created
+      });
+  }
 }]);
